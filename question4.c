6377ff16460d6d462cfa8911774e097f16c85bd6
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-13 16:48:30 -0500

    Create question4.c

---
+++ question4.c
@@ -0,0 +1,15 @@
+/*
+ * Tutorial 5 POSIX Thread Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2024, <Mostafa Abedi, Nathaniel Manoj, Calvin Reveredo>
+ * All rights reserved.
+ *
+*/
+#define _XOPEN_SOURCE 600 // required for barriers to work
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+int main(void)
+{
+  
+}
