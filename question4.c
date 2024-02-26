3c087cede144ee1bf98971f8286f035b2cd63fca
Mostafa-Abedi <79171483+Mostafa-Abedi@users.noreply.github.com>
2024-02-26 13:38:06 -0500

    Update question4.c

--- question4.c
+++ question4.c
@@ -5,11 +5,52 @@
  * All rights reserved.
  *
 */
-#define _XOPEN_SOURCE 600 // required for barriers to work
+#define _XOPEN_SOURCE 600
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+
+#define NUM_STUDENTS 10
+
+int total_grade = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void class_total(int grade) {
+    pthread_mutex_lock(&mutex);
+    total_grade += grade;
+    pthread_mutex_unlock(&mutex);
+}
+
+void *student_thread(void *arg) {
+    int grade = *((int *) arg);
+    class_total(grade);
+    pthread_exit(NULL);
+}
+
 int main(void)
 {
-  
+    int grades[NUM_STUDENTS];
+
+    printf("Enter grades for %d students:\n", NUM_STUDENTS);
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        printf("Student %d: ", i + 1);
+        scanf("%d", &grades[i]);
+    }
+
+    pthread_t threads[NUM_STUDENTS];
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        if (pthread_create(&threads[i], NULL, student_thread, (void *) &grades[i])) {
+            fprintf(stderr, "Error creating thread for student %d\n", i + 1);
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Total grade: %d\n", total_grade);
+
+    return 0;
 }
+
